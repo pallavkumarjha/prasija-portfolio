63a791c85f8017301326e9817145a407bc723599
Pallav <pallav@Pallavs-MacBook-Pro.local>
2025-09-20 21:12:53 +0530

    dfds

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
